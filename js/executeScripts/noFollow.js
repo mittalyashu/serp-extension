6e14d4aa2b2d3d24cc3530d8b4cef66eb954bba6
Yashu Mittal <mittalyashu77@gmail.com>
2024-04-05 13:29:17 +0530

    feat: allow disabling no-follow feature at domain level

--- js/executeScripts/noFollow.js
+++ js/executeScripts/noFollow.js
@@ -1,16 +1,17 @@
 var ID = "___serp_ext_nofollow"
 
 function addStyles() {
+  console.log('addStyles');
   const css = document.createElement("style");
   css.id = ID;
-  
+
   // To keep the styles update-to-date
-  let noFollowStyleTag = document.getElementById(ID)
+  let noFollowStyleTag = document.getElementById(ID);
   if (noFollowStyleTag) {
     noFollowStyleTag.parentNode.removeChild(noFollowStyleTag);
   }
 
-  css.innerHTML = "a[rel~='nofollow'] {outline: .14em dotted red !important;outline-offset: .2em;}a[rel~='nofollow'] img {outline: 2px dotted red !important;outline-offset: .2em;}"
+  css.innerHTML = "a[rel~='nofollow'] {outline: .14em dotted red !important;outline-offset: .2em;}a[rel~='nofollow'] img {outline: 2px dotted red !important;outline-offset: .2em;}";
 
   var head = document.head;
   head.appendChild(css);
@@ -19,7 +20,7 @@ function addStyles() {
 addStyles();
 
 function removeStyles() {
-  let noFollowStyleTag = document.getElementById(ID)
+  let noFollowStyleTag = document.getElementById(ID);
   if (noFollowStyleTag) {
     noFollowStyleTag.parentNode.removeChild(noFollowStyleTag);
   }
@@ -27,6 +28,6 @@ function removeStyles() {
 
 chrome.runtime.onMessage.addListener((request) => {
   if (request.message === "___serp_ext_stop_script") {
-    removeStyles()
+    removeStyles();
   }
 });
--- js/nofollow.js
+++ js/nofollow.js
@@ -1,47 +1,84 @@
-function getActiveTagId() {
+function getActiveTag() {
   return new Promise((resolve, reject) => {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       var activeTab = tabs[0];
       if (activeTab) {
-        resolve(activeTab.id);
+        resolve(activeTab);
       } else {
-        reject(new Error('No active tab found.'));
+        reject(new Error("No active tab found."));
       }
     });
   });
 }
 
 const noFollowBtn = document.getElementById("noFollowBtn");
-noFollowBtn.onclick = enableNoFollow;
+noFollowBtn.onclick = disableNoFollow;
 
 async function enableNoFollow() {
-  const activeTabId = await getActiveTagId();
-  if (!activeTabId) {
+  const activeTab = await getActiveTag();
+  if (!activeTab?.id) {
     return;
   }
 
   chrome.scripting.executeScript(
     {
-      target: { tabId: activeTabId },
+      target: { tabId: activeTab.id },
       files: ["js/executeScripts/noFollow.js"],
     },
     function () {
       noFollowBtn.innerHTML = "Disable";
       noFollowBtn.onclick = disableNoFollow;
+
+      const url = new URL(activeTab.url);
+      chrome.storage.local.remove(
+        `___serp_ext_disable-noFollow-${url.host}`,
+        function () {}
+      );
     }
   );
 }
 
 async function disableNoFollow() {
-  const activeTabId = await getActiveTagId();
-  if (!activeTabId) {
+  const activeTab = await getActiveTag();
+  if (!activeTab?.id) {
     return;
   }
 
-  chrome.tabs.sendMessage(activeTabId, {
+  chrome.tabs.sendMessage(activeTab.id, {
     message: "___serp_ext_stop_script",
   });
+  
+  const url = new URL(activeTab.url);
+  chrome.storage.local.set(
+    { [`___serp_ext_disable-noFollow-${url.host}`]: true },
+    function () {
+      console.log("Feature disable: [No follow]: " + url.host);
+    }
+  );
 
   noFollowBtn.innerHTML = "Enable";
   noFollowBtn.onclick = enableNoFollow;
 }
+
+async function checkState() {
+  const activeTab = await getActiveTag();
+  if (!activeTab?.id) {
+    return;
+  }
+
+  const url = new URL(activeTab.url);
+  const key = `___serp_ext_disable-noFollow-${url.host}`;
+  chrome.storage.local.get([key], function (result) {
+    console.log('res', result);
+    if (result.hasOwnProperty(key)) {
+      noFollowBtn.innerHTML = "Enable";
+      noFollowBtn.onclick = enableNoFollow;
+    } else {
+      noFollowBtn.innerHTML = "Disable";
+      noFollowBtn.onclick = disableNoFollow;
+    }
+  });
+}
+
+
+checkState()
